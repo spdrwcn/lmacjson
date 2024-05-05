ddd5612b6c936a21ba306587d153611f79329467
本子画师三岁半 <157886909+spdrwcn@users.noreply.github.com>
2024-05-06 00:16:45 +0800

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,10 @@
-//#![windows_subsystem = "windows"]  
-use std::result::Result;  
+//#![windows_subsystem = "windows"]   
   
 mod sn;  
 mod mac;  
 mod json;  
   
-fn main() -> Result<(), Box<dyn std::error::Error>> {  
+fn main() {  
     let serial_number = sn::get_bios_serial_number().unwrap_or_else(|err| {  
         eprintln!("Error fetching serial number: {}", err);  
         "Unknown".to_string()  
@@ -17,10 +16,12 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
         &wireless_mac,  
         &bluetooth_mac,  
     );  
-
+    println!("SN: {}", serial_number);  
+    println!("Wired MAC: {}", wired_mac);  
+    println!("Wireless MAC: {}", wireless_mac);  
+    println!("Bluetooth MAC: {}", bluetooth_mac);
     println!("JSON write status: {}", json_status);  
     println!("Press Enter to exit...");  
     std::io::stdin().read_line(&mut String::new()).unwrap();  
 
-    Ok(())  
 }
